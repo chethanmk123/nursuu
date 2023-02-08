3e0bed0d9e96515471834236470778eeb3f9d693
Chethan M K <Chethan M K@LAPTOP-FQFCD50K>
2023-02-08 12:00:18 +0530

    my code is completed

---
+++ chethan/src/Demo11/Demo11.java
@@ -0,0 +1,7 @@
+package Demo11;
+
+public class Demo11 
+{
+public final static String a=("hii");
+
+}
---
+++ chethan/src/Demo2/Demo2.java
@@ -0,0 +1,10 @@
+package Demo2;
+import Demo1.Demo1;
+public class Demo2 
+{
+public static void main(String[] args) 
+{
+	 int c = Demo1.a*Demo1.b;
+	System.out.println(c);
+}
+}
